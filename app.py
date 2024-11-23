912a5c0f62a897f46b8a8bf8831dd7485f9e1267
nancyoc <nancyoc21@gmail.com>
2024-11-23 13:45:42 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.balloons()
 # Show title and description.
 st.title("💬 nancy art")
 st.write(
-   "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
+   "Este es un chat para platicar de arte 🌻🎨🖼️. "
    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
 )
